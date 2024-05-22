e8433138f99c3b9a63d9b90b36aacd337267d59d
Thomas Dupoiron <tdupoiron@github.com>
2024-05-22 15:14:43 +0000

    chore: Update .gitignore and AWS Lambda configuration

--- aws/lambda/app.js
+++ aws/lambda/app.js
@@ -1,7 +1,45 @@
+const http = require('http');
+
 exports.handler = async (event) => {
-  const response = {
+  let response = {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda and Github!"),
   }
+  
+  console.log(response);
+  
+  const data = JSON.stringify(event);
+
+  const options = {
+    hostname: 'elastic.dupoiron.com',
+    port: 9200,
+    path: '/github/_doc',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+      'Authorization': 'Basic ' + Buffer.from('username:password').toString('base64')
+    }
+  }
+
+  const req = http.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+      process.stdout.write(d)
+    })
+  })
+
+  req.on('error', error => {
+    console.error(error)
+    response = {
+      statusCode: 500,
+      body: JSON.stringify("An error occurred: " + error.message),
+    }
+  })
+
+  req.write(data)
+  req.end()
+
   return response
 }
--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -14,14 +14,14 @@ module "github" {
   aws_region = var.aws_region
 
   github_organization = var.github_organization
-  github_webhook_url  = module.lambda.api_gateway_endpoint
+  github_webhook_url  = format("%s/%s/events", module.lambda.api_gateway_endpoint, module.lambda.api_gateway_stage_name)
 }
 
-module "elastic" {
-  source = "./modules/elastic"
+# module "elastic" {
+#   source = "./modules/elastic"
 
-  aws_owner             = var.aws_owner
-  aws_region            = var.aws_region
-  aws_availability_zone = var.aws_availability_zone
+#   aws_owner             = var.aws_owner
+#   aws_region            = var.aws_region
+#   aws_availability_zone = var.aws_availability_zone
 
-}
+# }
